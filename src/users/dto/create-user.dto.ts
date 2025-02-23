de83fdf708c5f4147496ff568a408384bc706b34
Gonzagadavid <gonzagadaviddev@gmail.com>
2025-02-23 14:52:42 -0300

    feat: create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,36 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'John Wick', description: 'The name of people' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    example: 'wick@email.com',
+    description: 'The email of people',
+  })
+  @IsString()
+  email: string;
+
+  @ApiProperty({
+    example: 36,
+    description: 'The age of people',
+  })
+  @IsNumber()
+  age: number;
+
+  @ApiProperty({
+    example: '9999999999',
+    description: 'The cell phone of people',
+  })
+  @IsString()
+  cellPhone: string;
+
+  @ApiProperty({
+    example: 'I am a retired person who loves dogs, I have already adopted two',
+    description: 'Description about people',
+  })
+  @IsString()
+  about: string;
+}
